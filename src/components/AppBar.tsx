d0b040b3a12653913bd195d3ca2fcd2a22704f33
Yan Mendes <cliente@172-0-20-49.lightspeed.iplsin.sbcglobal.net>
2025-10-13 10:49:53 -0300

    Ajustes finos

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router-dom"
+import { NavLink, useLocation, matchPath } from "react-router-dom"
 
 interface NavItem {
 	path: string
@@ -7,33 +7,42 @@ interface NavItem {
 }
 
 const navItems: NavItem[] = [
-	{ path: "/dashboard", label: "Dashboard", icon: "fi fi-ts-house-chimney" },
+	{ path: "/dashboard", label: "Dashboard", icon: "house-chimney" },
 	{ path: "/eventos", label: "Eventos", icon: "fi fi-ts-calendar" },
 	{ path: "/palestras", label: "Palestras", icon: "fi fi-ts-presentation" },
 	{ path: "/network", label: "Network", icon: "fi fi-ts-users" }
 ]
 
 export const AppBar = () => {
+	function checkIsActive(path: string) {
+		// eslint-disable-next-line react-hooks/rules-of-hooks
+		const { pathname } = useLocation()
+
+		return matchPath({ path, end: false }, pathname)
+	}
+	
 	return (
-		<nav className="fixed bottom-0 left-0 right-0 bg-surface-elevated border-t border-border z-50">
-			<div className="grid grid-cols-4 h-16">
-				{navItems.map((item) => (
-					<NavLink
-						key={item.path}
-						to={item.path}
-						className={({ isActive }) =>
-							`flex flex-col items-center justify-center gap-1 transition-smooth ${
-								isActive
-									? "text-primary"
-									: "text-muted-foreground hover:text-foreground"
-							}`
-						}
-					>
-						<i className={`${item.icon} text-xl`}></i>
-						<span className="text-xs font-medium">{item.label}</span>
-					</NavLink>
-				))}
-			</div>
-		</nav>
+		<>
+			<nav className="fixed bottom-0 left-0 right-0 mx-5 mb-3 bg-[rgba(15,15,15,.7)] border-t rounded-xl border-[#555555] backdrop-blur-sm z-50">
+				<div className="grid grid-cols-4 h-16">
+					{navItems.map((item) => (
+						<NavLink
+							key={item.path}
+							to={item.path}
+							className={({ isActive }) =>
+								`flex flex-col items-center justify-center gap-1 transition-smooth ${
+									isActive
+										? "text-primary"
+										: "text-muted-foreground hover:text-foreground"
+								}`
+							}
+						>
+							<i className={`fi fi-${checkIsActive(item.path) ? "ss-" : "ts-"}${item.icon} text-xl`}></i>
+							<span className={`text-xs font-${checkIsActive(item.path) ? "bold" : "medium"}`}>{item.label}</span>
+						</NavLink>
+					))}
+				</div>
+			</nav>
+		</>
 	)
 }
